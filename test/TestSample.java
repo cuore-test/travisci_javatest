985f7d5dc2781b51207fbe175d0ba16e8d6a5b36
hasegawa.masumi <hasegawa.masumi@Koji-PC.cuore.jp>
2014-10-31 17:10:15 +0900

    ymlファイル、TestSample.java修正

--- test/TestSample.java
+++ test/TestSample.java
@@ -1,9 +1,14 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
 
 
 public class TestSample {
+
+	public static void main(String[] args) {
+        JUnitCore.main(TestSample.class.getName());
+    }
 
 	@Test
 	public void testsample(){
